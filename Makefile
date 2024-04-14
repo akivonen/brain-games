bac300d0f58dbb8745bc3e4039089ae84687da76
Artjom Kivonen <kivonen@gmail.com>
2024-04-14 16:45:47 +0300

    config eslint, connecting CodeClimate

--- Makefile
+++ Makefile
@@ -2,5 +2,7 @@ install:
 	npm ci
 brain-games:
 	make brain-games
+make lint:
+	npx eslint .
 publish: 
 	npm publish --dry-run
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function cli () {
+export default function cli() {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
